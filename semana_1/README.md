61038bffe333126a808460f2698f664616a9352d
ALUMNO-N415 <ALUMNO-N415@UCSP.AULAB>
2023-03-16 19:26:24 -0500

    Agregando de nuevo

---
+++ semana_1/README.md
@@ -0,0 +1,7 @@
+# CCOMP2-2-2023-1-UCSP
+# GABO
+# Ciencia de la computación 1
+# Aprobaré con 17
+
+
+### editando desde github
---
+++ semana_1/helloword.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    cout << "Hola Mundo!" << endl;
+
+
+    return 0;
+
+}
